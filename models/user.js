e64801feb8b8ec88707ff8d88614632d29ee7404
Andrew Burlachenko <ap.burla4enko@gmail.com>
2023-03-24 13:38:00 +0300

    added registration/login/logout, authorization by JWT

---
+++ models/user.js
@@ -0,0 +1,65 @@
+const Joi = require("joi");
+const { Schema, model } = require("mongoose");
+
+const { handleMongooseError } = require("../helpers");
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: 6,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+  subscription: Joi.string().valid("starter", "pro", "business"),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const updateSubscription = Joi.object({
+  subscription: Joi.string()
+    .valid("starter", "pro", "business")
+    .required()
+    .messages({
+      "any.only":
+        "The subscription must have one of the following values 'starter', 'pro', 'business'",
+    }),
+});
+
+const schemasJoi = {
+  registerSchema,
+  loginSchema,
+  updateSubscription,
+};
+
+const User = model("user", userSchema);
+
+module.exports = { User, schemasJoi };
